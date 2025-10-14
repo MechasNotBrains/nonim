38c967aaf0427322fbacb231b9069de7d5aaafde
sOkam! <heysokam@noreply.codeberg.org>
2025-10-14 02:17:07 +0200

    chg: (WIP) Big refactor for nimble package builder
    Removes confy, which no longer supports Nim.

---
+++ src/nonim/fuzzer/nimgen.nimble
@@ -0,0 +1,31 @@
+#:____________________________________________________________________
+#  nim.gen  |  Copyright (C) Ivan Mar (sOkam!)  |  GPL-3.0-or-later  :
+#:____________________________________________________________________
+# Package Info
+packageName = "nimgen"
+version     = "0.0.0"
+author      = "heysokam"
+description = "codegen.nim | Random Code Generator for the Nim Programming Language"
+license     = "GPL-3.0-or-later"
+#___________________
+# Folders
+srcDir = "src"
+binDir = "bin"
+#___________________
+# Build requirements
+requires "nim >= 2.0.0"
+
+
+#_______________________________________
+# Tasks: Internal
+#_____________________________
+# @deps std
+import std/strformat
+import std/os
+#___________________
+task tests, "Internal:  Runs all unit tests of the project.":
+  if dirExists("./bin/.tests"): rmDir("./bin/.tests")
+  for testFile in os.walkDirRec("./src", yieldFilter= {pcFile}, relative= true):
+    if not testFile.endsWith("_test.nim"): continue
+    selfExec &"c {testFile} --outDir:./bin/.tests"
+
--- src/nonim/fuzzer/src/nimgen.nim
+++ src/nonim/fuzzer/src/nimgen.nim
@@ -1,45 +1,18 @@
-#:______________________________________________________________________
-#  nim.gen  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
-#:______________________________________________________________________
-# @deps std
-from std/os import nil
-from std/strutils import join, toHex
-from std/strformat import `&`
-# @deps nim.gen
-import ./random
-import ./gen
+#:____________________________________________________________________
+#  nim.gen  |  Copyright (C) Ivan Mar (sOkam!)  |  GPL-3.0-or-later  :
+#:____________________________________________________________________
+from std/os import commandLineParams
+
 
 #_______________________________________
-# @section nim.gen Logging
+# @section nim.gen Library API
 #_____________________________
-func info *(msg :varargs[string, `$`]) :void=
-  debugEcho "[nim.gen] " & msg.join(" ")
 
 
 #_______________________________________
 # @section nim.gen Entry Point
 #_____________________________
-proc run (args :seq[string])=
-  doAssert args.len in {2,3}
-  let choice = args[0]
-  let file   = args[1]
-  let dir    = os.splitFile(file).dir
-  let seed   = if args.len > 2: args[2] else: random.integer().toHex()
-  random.init(seed)
-  # Validate Arguments
-  doAssert choice in ["all", "variable", "proc"]
-  doAssert not os.fileExists(file)
-  # Report to CLI
-  info(&"Generating {choice} code at {file} with seed {seed}")
-  # Run the generator
-  let code = case choice # FIX: Pass allowBlocks option from cli
-  of "variable" : gen.variable()
-  of "proc"     : gen.procs()
-  else          : gen.nim()
-  # Write to the file
-  if not os.dirExists(dir): os.createDir dir
-  file.writeFile(code)
-  when defined(debug): echo code
-#___________________
-when isMainModule: nimgen.run(os.commandLineParams())
+when isMainModule:
+  from ./nimgen/cli import nil
+  cli.run(os.commandLineParams())
 
