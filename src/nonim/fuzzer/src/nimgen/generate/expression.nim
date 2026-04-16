44c596380cac871ab61744de5c9747e27da6dacf
sOkam! <heysokam@noreply.codeberg.org>
2026-04-16 08:19:50 +0200

    new: `expression/curly` generator

--- src/nonim/fuzzer/src/nimgen/generate/expression.nim
+++ src/nonim/fuzzer/src/nimgen/generate/expression.nim
@@ -164,6 +164,18 @@ func bracket *(
 
 
 #_______________________________________
+# @section Expression Generation: Curly (Set Constructor)
+#_____________________________
+func curly *(
+    info  : TLineInfo;
+    depth : int       = 0;
+    args  : seq[PNode] = expression.arguments(info, depth= depth);
+  ) :PNode=
+  result = newNodeI(nkCurly, info)
+  for arg in args: result.add(arg)
+
+
+#_______________________________________
 # @section Expression Generation: Entry Point
 #_____________________________
 func random *(info :TLineInfo; T :string= R.typename(); depth :int= 0) :PNode=
