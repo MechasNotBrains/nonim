ac720e9ac025faf1c62a6720a0337529c2ad68b3
sOkam <7308253+heysokam@users.noreply.github.com>
2026-05-17 01:43:06 +0200

    bld: Fix `ast/json` unit tests for using `minitest`

--- src/nonim/ast/json_test.nim
+++ src/nonim/ast/json_test.nim
@@ -2,25 +2,32 @@
 #  nonim  |  Copyright (C) Ivan Mar (sOkam!)  |  MPL-2.0  :
 #:_________________________________________________________
 from std/os import splitPath, `/`, fileExists, pcDir, parentDir
-from std/strutils import startsWith
-from std/sugar import capture
 import minitest
 import ./json
+from astTF import nil
 
 const samplesDirectory {.strdefine.} = currentSourcePath().parentDir()/".."/".."/".."/"bin"/".lib"/"astTF"/"samples"
 
+type TestCase = object
+  title   :string
+  parsed  :astTF.astTF
+
+var cases :seq[TestCase]
+for category in os.walkDir(samplesDirectory):
+  if category.kind != pcDir: continue
+  if category.path.splitPath().tail == "invalid": continue
+  for sample in os.walkDir(category.path):
+    if sample.kind != pcDir: continue
+    let astFile = sample.path / "ast.atf"
+    if not fileExists(astFile): continue
+    let name = category.path.splitPath().tail & "/" & sample.path.splitPath().tail
+    cases.add TestCase(
+      title: "must be able to parse " & name,
+      parsed: astFile.readFile().fromJson(),
+    )
+
 describe "nonim.ast.json | parse all samples":
-  for category in os.walkDir(samplesDirectory):
-    if category.kind != pcDir: continue
-    if category.path.splitPath().tail == "invalid": continue
-    for sample in os.walkDir(category.path):
-      if sample.kind != pcDir: continue
-      let astFile = sample.path / "ast.atf"
-      if not fileExists(astFile): continue
-      let name = category.path.splitPath().tail & "/" & sample.path.splitPath().tail
-      capture astFile, name:
-        it "must be able to parse " & name, proc() =
-          let content = astFile.readFile()
-          let parsed  = content.fromJson()
-          parsed.data.modules.len.eq(1)
+  for index in 0 ..< cases.len:
+    it cases[index].title, proc() =
+      cases[index].parsed.data.modules.len.eq(1)
 
