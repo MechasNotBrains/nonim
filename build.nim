fb002511b0d7c12494e0ba6cb3366f87278147f8
sOkam <7308253+heysokam@users.noreply.github.com>
2026-05-17 00:07:08 +0200

    bld: Add `astTF` dependency to `nonim` minibuild builder

--- build.nim
+++ build.nim
@@ -2,4 +2,7 @@
 #  nonim  |  Copyright (C) Ivan Mar (sOkam!)  |  MPL-2.0  :
 #:_________________________________________________________
 from minibuild as B import build
-B.program("nonim.nim").build(run=true)
+const astTF = B.dependency("astTF", "https://codeberg.org/heysokam/astTF", path= "spec")
+B.program("nonim.nim",
+   deps = @[astTF],
+  ).build(run=true)
