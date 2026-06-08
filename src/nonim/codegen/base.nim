d638bc9dd6a31f3b05565007d2b324dbdf5e74fa
sOkam <7308253+heysokam@users.noreply.github.com>
2026-06-08 08:34:51 +0200

    new: all: Better codegen.base tools for all langs

--- src/nonim/codegen/base.nim
+++ src/nonim/codegen/base.nim
@@ -4,11 +4,81 @@
 ## Shared codegen helpers used by all backends.
 #_______________________________________________________________|
 from std/options import isSome, isNone, get, Option
+from std/sequtils import toSeq
+from std/strutils import join
 import ../ast as astTF
+from ./output import Output, string
+
+#_______________________________________
+# @section Error Management
+#_____________________________
+type BaseCodegenError = object of CatchableError
+#___________________
+proc fail *(
+    err  : typedesc;
+    msg  : static string;
+    args : varargs[string, `$`];
+  ) :void {.noreturn.}= raise newException(err, (@["[nonim.codegen]", msg] & args.toSeq).join(" "))
 
 
+#_______________________________________
+# @section Depth Management
+#_____________________________
 func node_depth *(ast :astTF.Ast; depth_id :Option[astTF.Id]) :int=
-  if depth_id.isNone: return 0
-  let depth = ast.data.depths.get[depth_id.get]
-  if depth.indent.isSome: return int(depth.indent.get)
-  return 0
+  result = 0
+  if depth_id.isNone: return
+  let depth = ast.depth(depth_id.get)
+  if depth.indent.isSome: result = depth.indent.get.int
+
+
+#_______________________________________
+# @section Source String Helpers
+#_____________________________
+func type_name *(
+    ast    : astTF.Ast;
+    module : astTF.Id;
+    id     : astTF.Id;
+  ) :string=
+  result   = "_"
+  let name = astTF.type_name(ast, id)
+  if not name.isSome: fail BaseCodegenError, "base.type_name: Tried to access the name of a type that has no name."
+  result = ast.source(module, name.get)
+
+
+#_______________________________________
+# @section Formatting
+#_____________________________
+const format_Newline * = "\n"
+const format_Space   * = " "
+const format_Tab     * = format_Space & format_Space
+#___________________
+func format_before *(
+    ast    : astTF.Ast;
+    module : astTF.Id;
+    id     : astTF.Id;
+    Out    : var Output;
+    indent : string = " ";
+  ) :void=
+  let fmt = ast.format(id)
+  for _ in 0..<fmt.newlines.get(0) : Out.string(module, format_Newline, output.Target.definition)
+  for _ in 0..<fmt.indent.get(0)   : Out.string(module, format_Tab,     output.Target.definition)
+  for _ in 0..<fmt.before.get(0)   : Out.string(module, format_Space,   output.Target.definition)
+#___________________
+func format_after *(
+    ast    : astTF.Ast;
+    module : astTF.Id;
+    id     : astTF.Id;
+    Out    : var Output;
+  ) :void=
+  let fmt = ast.format(id)
+  for _ in 0..<fmt.after.get(0): Out.string(module, format_Space, output.Target.definition)
+#___________________
+func format_comment *(
+    ast    : astTF.Ast;
+    module : astTF.Id;
+    id     : astTF.Id;
+    Out    : var Output;
+  ) :void=
+  let fmt = ast.format(id)
+  for _ in 0..<fmt.comment.get(0): Out.string(module, format_Space, output.Target.definition)
+
