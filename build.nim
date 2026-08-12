3db7a741d7642158268618f682a8a78fbe6789a7
sOkam <7308253+heysokam@users.noreply.github.com>
2026-08-12 18:56:42 +0200

    bld: Add tests to the `minibuild` builder

--- build.nim
+++ build.nim
@@ -2,8 +2,13 @@
 #  nonim  |  Copyright (C) Ivan Mar (sOkam!)  |  MPL-2.0  :
 #:_________________________________________________________
 from minibuild as B import build
+# Dependencies
 const astTF     = B.dependency("astTF",     "https://codeberg.org/heysokam/astTF", path= "spec")
 const minibuild = B.dependency("minibuild", "https://github.com/MechasNotBrains/minibuild")
+# Tests
+const minitest  = B.dependency("minitest",  "https://codeberg.org/heysokam/minitest")
+B.unit_test("nonim/test.nim", deps= @[astTF, minibuild, minitest]).build(run=true)
+# Binaries
 B.program("nonim/nimcheck.nim").build()
 B.program("nonim/nimgen.nim").build()
 B.program("nonim/minc.nim", deps= @[astTF, minibuild]).build()
